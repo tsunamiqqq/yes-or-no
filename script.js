e361ba4a25c92230f49b187a97af125edb2c4f22
Feyzi777 <116453634+Feyzi777@users.noreply.github.com>
2022-10-24 18:17:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const text = document.querySelector('.main-text')
+function succer() {
+window.location.href=('steam://run/570');
+}
+function fail() {
+text.innerHTML = 'Кому ты врёшь, твой путь только к "Yes"'
+}
